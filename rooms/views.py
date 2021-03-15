0fa3ca44f2fad4842fb405c99b45d3413dc1d4d9
Minchoul2 <sny2128@gmail.com>
2021-03-15 20:46:57 +0900

    #11.8 ListView - get_context_data 기본

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.views.generic import ListView
 from . import models
 
@@ -9,3 +10,11 @@ class HomeView(ListView):
     paginate_by = 10
     paginate_orphans = 6
     ordering = "created"
+    context_object_name = "rooms"  # object_list 대신 rooms
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+
+        return context  # context는 템플릿으로 가는 변수
