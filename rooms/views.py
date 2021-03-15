273a6e4a26096d5c2d0cd3fb5b4ddc032a998a06
Minchoul2 <sny2128@gmail.com>
2021-03-15 20:07:15 +0900

    #11.7 ListView를 이용해서 홈 만들기(제일 간단한 방법)

--- rooms/views.py
+++ rooms/views.py
@@ -1,22 +1,11 @@
-from math import ceil
-from datetime import datetime
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
-
-# from django.http import HttpResponse
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):  # 뷰 이름은 urls.py에서 path 의 이름과 같아야함
-    page = request.GET.get("page", 1)  # 두번째 인자는 default 값, page사용시 필요
-    room_list = models.Room.objects.all()  # 사실 쿼리셋을 호출하는 것은 Lazy하게 한다!!
-    paginator = Paginator(room_list, 10, orphans=5)
-    try:
-        rooms = paginator.page(int(page))  # get_page는 보통 다 갖춰짐 // page는 에러를 컨트롤 해줘야함
-        return render(
-            request,
-            "rooms/home.html",
-            context={"page": rooms},
-        )
-    except EmptyPage:
-        return redirect("/")  # 처음으로 돌아가라
+class HomeView(ListView):
+    """ Home View Define"""
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 6
+    ordering = "created"
