485e8e8379b75bda52c95525cb5e3037f8f1151c
Minchoul2 <sny2128@gmail.com>
2021-03-15 15:47:41 +0900

    #11.1 페이지 만들기

--- rooms/views.py
+++ rooms/views.py
@@ -6,7 +6,11 @@ from . import models
 
 
 def all_rooms(request):  # 뷰 이름은 urls.py에서 path 의 이름과 같아야함
-    all_rooms = models.Room.objects.all()
+    page = int(request.GET.get("page", 1))
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = models.Room.objects.all()[offset:limit]
     return render(
         request,
         "home/home.html",  # 템플릿이름은 반드시 templates폴더의 있는 파일명과 같아야함 (요구사항)
