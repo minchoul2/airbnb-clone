88d5dee8fda233551b5dc6fbbee21df16721c071
Minchoul2 <sny2128@gmail.com>
2021-03-15 17:48:09 +0900

    #11.5 get_page와 page 를 이용하여 수정

--- rooms/views.py
+++ rooms/views.py
@@ -8,13 +8,13 @@ from . import models
 
 
 def all_rooms(request):  # 뷰 이름은 urls.py에서 path 의 이름과 같아야함
-    page = request.GET.get("page")
+    page = request.GET.get("page", 1)  # 두번째 인자는 default 값, page사용시 필요
     room_list = models.Room.objects.all()  # 사실 쿼리셋을 호출하는 것은 Lazy하게 한다!!
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    print(vars(rooms))
+    paginator = Paginator(room_list, 10, orphans=5)
+    rooms = paginator.page(int(page))  # get_page는 보통 다 갖춰짐 // page는 에러를 컨트롤 해줘야함
+
     return render(
         request,
         "home/home.html",  # 템플릿이름은 반드시 templates폴더의 있는 파일명과 같아야함 (요구사항)
-        context={"rooms": rooms},  # html에 사용할 변수
+        context={"page": rooms},  # html에 사용할 변수
     )
