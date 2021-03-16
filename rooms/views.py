d2f95f939abe864a8f630357ccdabfaf9af5813a
Minchoul2 <sny2128@gmail.com>
2021-03-16 15:32:59 +0900

    #12.2 room_detail에 간단한 정보 출력

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,6 @@
-from django.utils import timezone
-from django.shortcuts import render
 from django.views.generic import ListView
+from django.urls import reverse
+from django.shortcuts import render, redirect
 from . import models
 
 
@@ -15,5 +15,8 @@ class HomeView(ListView):
 
 
 def room_detail(request, pk):
-    print(pk)
-    return render(request, r"rooms/detail.html")
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, r"rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        return redirect(reverse("core:home"))
