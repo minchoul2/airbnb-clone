cfd74ee755763b8774899c3bda267052a2fce0dd
Minchoul2 <sny2128@gmail.com>
2021-03-17 20:44:16 +0900

    #13.6 Filtering Like a Boss part Two

--- rooms/views.py
+++ rooms/views.py
@@ -27,14 +27,13 @@ def search(request):
     city = str.capitalize(city)
     country = request.GET.get("country", "KR")
     room_type = int(request.GET.get("room_type", 0))
-
-    price = request.GET.get("price", 0)
-    guests = request.GET.get("guests", 0)
-    beds = request.GET.get("beds", 0)
-    bathrooms = request.GET.get("bathrooms", 0)
-    bedrooms = request.GET.get("bedrooms", 0)
-    instant = request.GET.get("instant", False)
-    super_host = request.GET.get("super_host", False)
+    price = int(request.GET.get("price", 0) or 0)
+    guests = int(request.GET.get("guests", 0) or 0)
+    beds = int(request.GET.get("beds", 0) or 0)
+    bathrooms = int(request.GET.get("bathrooms", 0) or 0)
+    bedrooms = int(request.GET.get("bedrooms", 0) or 0)
+    instant = bool(request.GET.get("instant", False))
+    superhost = bool(request.GET.get("superhost", False))
 
     s_amenities = request.GET.getlist("amenities")
     s_facilites = request.GET.getlist("facilites")
@@ -52,7 +51,7 @@ def search(request):
         "s_amenities": s_amenities,
         "s_facilites": s_facilites,
         "instant": instant,
-        "super_host": super_host,
+        "superhost": superhost,
     }
     room_types = models.RoomType.objects.all()
     amenities = models.Amenity.objects.all()
@@ -71,9 +70,37 @@ def search(request):
         filter_args["city__startswith"] = city
     # country 필터
     filter_args["country"] = country
-    # room_type 필터
+    # room_type(foreign key) 필터
     if room_type != 0:
         filter_args["room_type__pk"] = room_type
+    # price
+    if price != 0:
+        filter_args["price__lte"] = price
+
+    # guest
+    if guests != 0:
+        filter_args["guests__gte"] = guests
+    # bedrooms
+    if bedrooms != 0:
+        filter_args["bedrooms__gte"] = bedrooms
+    # bathrooms
+    if bathrooms != 0:
+        filter_args["bathrooms__gte"] = bathrooms
+    # beds
+    if beds != 0:
+        filter_args["beds__gte"] = beds
+
+    if instant is True:
+        filter_args["istant_book"] = True
+    if superhost is True:
+        filter_args["host__superhost"] = True  # superhost는 foreign key니까 모델안에서 끌고와야함
+
+    if len(s_amenities) > 0:
+        for s_a in s_amenities:
+            filter_args["amenity__pk"] = int(s_a)
+    if len(s_facilites) > 0:
+        for s_a in s_facilites:
+            filter_args["facility__pk"] = int(s_a)
 
     rooms = models.Room.objects.filter(**filter_args)
 
