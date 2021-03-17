77f8e8d081371eea2de03cf666824af85240b39f
Minchoul2 <sny2128@gmail.com>
2021-03-17 14:45:33 +0900

    #13.0 Template, Form, Url, Setup

--- rooms/urls.py
+++ rooms/urls.py
@@ -5,5 +5,10 @@ from . import views
 app_name = "rooms"
 
 urlpatterns = [
-    path("<int:pk>", views.RoomDetail.as_view(), name="detail")
-]  # room_list.html의 url 태그에 쓰일 namespace room:detail
+    path(
+        "<int:pk>",
+        views.RoomDetail.as_view(),
+        name="detail",
+    ),  # room_list.html의 url 태그에 쓰일 namespace room:detail
+    path("search/", views.search, name="search"),
+]
--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
 from . import models
 
 
@@ -17,4 +18,10 @@ class RoomDetail(DetailView):
     """RoomDetail Definition"""
 
     model = models.Room
-    pk_url_kwarg = "pk"  # pk이름을 바꾸는 것  pk_url_kwarg = "potato" <= urls.py path("<int:potato>", views.RoomDetail.as_view(), name="detail")
+    pk_url_kwarg = "pk"  # pk이름을 바꾸는 것  pk_url_kwarg = "potato" <= urls.py pa th("<int:potato>", views.RoomDetail.as_view(), name="detail")
+
+
+def search(request):
+    city = request.GET.get("city")
+    city = str.capitalize(city)
+    return render(request, "rooms/search.html", {"city": city})
