d4e5b7c825dfedc1d5e18b3fea02c87304107d42
Minchoul2 <sny2128@gmail.com>
2021-03-17 22:29:55 +0900

    #13.9 Forms by 클래스

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 from django.shortcuts import render
 from django_countries import countries
 from . import models, forms
@@ -22,12 +22,76 @@ class RoomDetail(DetailView):
     pk_url_kwarg = "pk"  # pk이름을 바꾸는 것  pk_url_kwarg = "potato" <= urls.py pa th("<int:potato>", views.RoomDetail.as_view(), name="detail")
 
 
-def search(request):
+class SearchView(View):# 클래스 뷰 기반으로 옮김(def search(requst)->class ss(View):def get(self, requst))
+    def get(self,request):
+        country = forms.SearchForm(request.GET)
 
-    form = forms.SearchForm()
+            if country:
+                form = forms.SearchForm(request.GET)
+                if form.is_valid():
+                    city = form.cleaned_data.get("city")
+                    country = form.cleaned_data.get("country")
+                    room_type = form.cleaned_data.get("room_type")
+                    price = form.cleaned_data.get("price")
+                    guests = form.cleaned_data.get("guests")
+                    bedrooms = form.cleaned_data.get("bedrooms")
+                    beds = form.cleaned_data.get("beds")
+                    baths = form.cleaned_data.get("baths")
+                    instant_book = form.cleaned_data.get("instant_book")
+                    superhost = form.cleaned_data.get("superhost")
+                    amenities = form.cleaned_data.get("amenities")
+                    facilites = form.cleaned_data.get("facilites")
 
-    return render(
-        request,
-        "rooms/search.html",
-        {"form": form},  # 위 딕셔너리들을 다 풀어 놓을 거야
-    )
+                    filter_args = {}
+
+                    # city 필터
+                    if city != "Antwhere":
+                        filter_args["city__startswith"] = city
+                    # country 필터
+                    filter_args["country"] = country
+                    # room_type(foreign key) 필터
+                    if room_type is not None:
+                        filter_args["room_type"] = room_type
+                    # price
+                    if price is not None:
+                        filter_args["price__lte"] = price
+
+                    # guest
+                    if guests is not None:
+                        filter_args["guests__gte"] = guests
+                    # bedrooms
+                    if bedrooms is not None:
+                        filter_args["bedrooms__gte"] = bedrooms
+                    # bathrooms
+                    if baths is not None:
+                        filter_args["bathrooms__gte"] = baths
+                    # beds
+                    if beds is not None:
+                        filter_args["beds__gte"] = beds
+
+                    if instant_book is True:
+                        filter_args["istant_book"] = True
+                    if superhost is True:
+                        filter_args[
+                            "host__superhost"
+                        ] = True  # superhost는 foreign key니까 모델안에서 끌고와야함
+
+                    for a in amenities:
+                        filter_args["amenity"] = a
+
+                    for f in facilites:
+                        filter_args["facility"] = f
+                    print(filter_args)
+                    rooms = models.Room.objects.filter(**filter_args)
+
+            else:
+                form = forms.SearchForm()
+
+            return render(
+                request,
+                "rooms/search.html",
+                {"form": form, "rooms": rooms},  # 위 딕셔너리들을 다 풀어 놓을 거야
+            )
+
+
+    
